7fefad65c5e99e56715994677be27caea6845543
Hak1e <hakie.notiko@gmail.com>
2026-06-17 22:57:46 +0400

    Edited config for build via yarn

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,18 +1,15 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-    ft = { "markdown" },
-    -- Use this command for build: :Lazy build markdown-preview.nvim
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    init = function()
-      vim.g.mkdp_auto_start = 1
-      vim.g.mkdp_auto_close = 1
-      vim.g.mkdp_refresh_slow = 0
-      vim.g.mkdp_browser = "firefox" -- можно указать "firefox" или "brave"
-      vim.g.mkdp_theme = "dark"
-    end,
-  },
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+		vim.g.mkdp_auto_start = 1
+		vim.g.mkdp_auto_close = 1
+		vim.g.mkdp_refresh_slow = 0
+		vim.g.mkdp_browser = "firefox" -- можно указать "firefox" или "brave"
+		vim.g.mkdp_theme = "dark"
+	end,
+	ft = { "markdown" },
 }
+
